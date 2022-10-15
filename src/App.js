2308d8ec7d26893b0ca49475d14debced44e34da
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-10-15 23:45:20 +0300

    fixed navbar import

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Navbar from './components/navbar.component';
+
 import ExerciseList from './components/exercises-list.component';
 import EditExercise from './components/edit-exercises.component';
 import CreateExercise from './components/create-exercise.component';
